ac5f7bcf808d46fa873e48c6057ff334ad3b520b
EXA72224 <jperez@cfotechlatam.com>
2024-08-08 19:02:32 -0300

    Added mobile viewport support

--- src/components/categoryGuide/CategoryGuide.tsx
+++ src/components/categoryGuide/CategoryGuide.tsx
@@ -5,7 +5,7 @@ interface Props {  }
 
 const CategoryGuide = ({  }: Props) => {
   return (
-    <div className="flex items-center justify-center gap-5 flex-wrap mx-auto my-5 px-5 py-3 max-w-[60vw] border-2 border-dashed border-slate-500 rounded-sm text-slate-800 font-semibold">
+    <div className="grid grid-cols-2 md:flex md:items-center md:justify-center md:gap-5 md:flex-wrap mx-auto my-5 px-5 py-3 md:max-w-[60vw] border-2 border-dashed border-slate-500 rounded-sm text-slate-800 font-semibold">
       { Object.values(CATEGORIES).map( category => (
           <CategoryElement
             name={category.label}
