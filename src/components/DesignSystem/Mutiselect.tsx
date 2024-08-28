4f3500ff72c08e5784b42fa4d060d4c2572813e9
EXA72224 <jperez@cfotechlatam.com>
2024-08-28 19:22:50 -0300

    Fixed MultiSelect to work with react hook form

--- src/components/DesignSystem/Mutiselect.tsx
+++ src/components/DesignSystem/Mutiselect.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Select, Label, Button, Popover, ListBox, ListBoxItem, SelectValue, Key } from 'react-aria-components'
 import { IoChevronDownSharp } from "react-icons/io5"
 import { FaCheck } from 'react-icons/fa'
@@ -6,15 +5,18 @@ import { LuAsterisk } from 'react-icons/lu'
 import { twMerge } from 'tailwind-merge'
 import { SelectOption } from '../../models/formTypes'
 import { useController, UseFormReturn } from 'react-hook-form'
+import InfoIcon from './InfoIcon'
 
 interface Props {
   methods: UseFormReturn<any>
   name: string
   options: SelectOption[]
   label: any
-  onChange?: (value: string) => void
-  onBlur?: (value: string) => void
+  infoText?: string
+  onChange?: (value: string[]) => void
+  onBlur?: (value: string[]) => void
   required?: boolean
+  disabled?: boolean
   placeholder?: string
 }
 
@@ -23,32 +25,22 @@ const MultiSelect = ({
   name,
   options,
   label,
+  infoText,
   onChange,
   onBlur,
   required = false,
+  disabled = false,
   placeholder = 'Seleccionar',
 }: Props) => {
-  const [selectedValues, setSelectedValues] = useState<string[]>([])
   const { control } = methods
   const { field, fieldState } = useController({ 
     name,
     control,
+    defaultValue: [],
     rules: {
       required: { value: required, message: 'Required field' },
-      // validate //TODO: rules para validar
     }
   })
-
-  // const handleSelectionChange = (key: Key) => {
-  //   const itemKey = String(key)
-
-  //   if(selectedValues.includes(itemKey)) {
-  //     const filteredValues = selectedValues.filter( el => el !== itemKey )
-  //     setSelectedValues(filteredValues)
-  //   } else {
-  //     setSelectedValues([...selectedValues, itemKey])
-  //   }
-  // }
 
   const handleSelectionChange = (key: Key) => {
     const itemKey = String(key)
@@ -57,18 +49,17 @@ const MultiSelect = ({
       : [...field.value, itemKey]
 
     field.onChange(newValues)
-    if(onChange) onChange(itemKey)
+    if(onChange) onChange(newValues)
   }
 
   return (
     <Select
       name={field.name}
-      selectedKey={field.value}
       onSelectionChange={handleSelectionChange}
       className="inline-flex flex-col min-w-[5rem]"
-      onBlur={(e: any) => {
+      onBlur={() => {
         field.onBlur()
-        if(onBlur) onBlur(e.target.value)
+        if(onBlur) onBlur(field.value)
       }}
     >
       <span className="flex items-start gap-1">
@@ -78,6 +69,7 @@ const MultiSelect = ({
           </Label>
         }
         { required && <LuAsterisk className="text-rose-500" size={10} /> }
+        { infoText && <InfoIcon id={name} text={infoText} /> }
       </span>
 
       <Button
@@ -87,14 +79,14 @@ const MultiSelect = ({
           border rounded-md outline-none select-none
           bg-slate-100 border-slate-300 hover:border-slate-400 focus:border-sky-500 focus:ring-1 focus:ring-sky-300
           transition-all duration-200`,
-          // disabled && 'bg-gray-100 cursor-not-allowed',
+          disabled && 'bg-gray-100 cursor-not-allowed',
           fieldState?.error && 'border-rose-500 ring-1 ring-rose-300 focus:border-red-700 focus:ring-red-500 hover:border-red-600',
         )}
       >
         <SelectValue>
-          {selectedValues.length === 0
+          {field.value.length === 0
             ? <span className="text-slate-400 font-light">{placeholder}</span>
-            : selectedValues.length > 1 ? `${selectedValues.length} items selected` : `${selectedValues.length} item selected`
+            : `${field.value.length} item${field.value.length > 1 ? 's' : ''} selected`
           }
         </SelectValue>
         <span className="flex">
@@ -111,20 +103,21 @@ const MultiSelect = ({
 
       <Popover className="w-[--trigger-width]" placement="bottom">
         <ListBox selectionBehavior="toggle" className="p-1 m-0 bg-white rounded-md border border-slate-300 text-slate-600 focus:outline-none">
-          { options.map( option => (
+          {options.map(option => (
             <ListBoxItem
               key={crypto.randomUUID()}
               id={option.value}
-              className={({ isSelected, isDisabled }) => (
+              textValue={option.label}
+              className={({ isDisabled }) => (
                 twMerge(
                   `w-full flex items-center justify-between p-2 rounded-md text-sm cursor-pointer select-none hover:bg-sky-100 focus:outline-none focus:bg-sky-100`,
-                  isSelected && 'text-sky-600 font-medium',
+                  field.value.includes(option.value) && 'text-sky-600 font-medium',
                   isDisabled && 'bg-gray-100 text-gray-400 italic pointer-events-none',
                 )
               )}
             >
               <span>{option.label}</span>
-              {selectedValues.includes(option.value) && <FaCheck />}
+              {field.value.includes(option.value) && <FaCheck />}
             </ListBoxItem>
           ))}
         </ListBox>
