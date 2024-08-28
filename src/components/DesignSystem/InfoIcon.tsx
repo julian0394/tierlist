2eadf7a2c0ffb739be299368d8dab48bc43636d6
EXA72224 <jperez@cfotechlatam.com>
2024-08-28 19:20:09 -0300

    Added new props to InfoIcon component

--- src/components/DesignSystem/InfoIcon.tsx
+++ src/components/DesignSystem/InfoIcon.tsx
@@ -4,13 +4,22 @@ import { Tooltip } from "react-tooltip"
 interface Props {
   text: string
   id: string
+  clickable?: boolean
+  openOnClick?: boolean
 }
 
-const InfoIcon = ({ text, id }: Props) => {
+const InfoIcon = ({ text, id, clickable = false, openOnClick = false }: Props) => {
   return (
     <>
       <span id={`tooltip-${id}`} className="p-1 text-sky-600"><FaInfoCircle size={12} /></span>
-      <Tooltip className="z-50 " style={{backgroundColor: '#000', borderRadius: '5px', maxWidth: '15rem'}} anchorSelect={`#tooltip-${id}`} place="top">
+      <Tooltip
+        anchorSelect={`#tooltip-${id}`}
+        place="top"
+        clickable={clickable}
+        openOnClick={openOnClick}
+        className="z-50"
+        style={{backgroundColor: '#000', borderRadius: '5px', maxWidth: '15rem'}}
+      >
         {text}
       </Tooltip>
     </>
